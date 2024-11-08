7062b749aa2265e0b7a90050904d4e7b56cb15f0
jhasankbharadwaj <218r5a6711@gmail.com>
2024-11-08 17:27:23 +0530

    first commit

---
+++ CareConnectApi/CareConnect.Service.WelBeingApi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CareConnect.Service.WelBeingApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
